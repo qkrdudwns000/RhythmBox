624cda1ff63a960cff056275d97d2c995e6513b0
qkrdudwns000 <114732265+qkrdudwns000@users.noreply.github.com>
2023-02-25 01:24:16 +0900

    리소스다운 및 노트 UI구현

---
+++ Assets/02.Scripts/Manager/NoteManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform tfNoteAppear;
+    [SerializeField] GameObject goNote;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
